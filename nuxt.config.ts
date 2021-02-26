488eced7d447c30d8edf93e03a7ed6802778e86c
Anthony Fu <anthonyfu117@hotmail.com>
2021-02-26 18:24:11 +0800

    chore: bump nuxt-vite

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,33 @@
+import { resolve } from 'path'
 import { NuxtConfig } from '@nuxt/types'
+import ViteComponents from 'vite-plugin-components'
+import WindiCSS from 'vite-plugin-windicss'
 
 const config: NuxtConfig = {
   buildModules: [
     'nuxt-vite',
   ],
   plugins: [
-    '~/plugins/main.js',
+    '~/plugins/main.ts',
   ],
+  vite: {
+    plugins: [
+      ViteComponents({
+        dirs: [
+          resolve('./components'),
+        ],
+        transformer: 'vue2',
+      }),
+      WindiCSS({
+        scan: {
+          dirs: [
+            resolve('./pages'),
+            resolve('./components'),
+          ],
+        },
+      }),
+    ],
+  },
 }
 
 export default config
